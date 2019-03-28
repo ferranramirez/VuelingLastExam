f9d230671ef4a098e61b3a249d08de793345c6ec
Ferran Ramírez <ferranramireznavajon@gmail.com>
2019-03-28 14:56:22 +0100

    First Final Commit

---
+++ VuelingExam.Domain.Impl.Services/Exceptions/VuelingExamDomainException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VuelingExam.Domain.Impl.Services.Exceptions
+{
+    [SerializableAttribute]
+    public class VuelingExamDomainException : Exception
+    {
+        public VuelingExamDomainException()
+        {
+        }
+
+        public VuelingExamDomainException(string message) : base(message)
+        {
+        }
+
+        public VuelingExamDomainException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ VuelingExam.Infrastructure.Impl.Repository/Exceptions/VuelingExamInfrastructureException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VuelingExam.Infrastructure.Impl.Repository.Exceptions
+{
+    [SerializableAttribute]
+    public class VuelingExamInfrastructureException : Exception
+    {
+        public VuelingExamInfrastructureException()
+        {
+        }
+
+        public VuelingExamInfrastructureException(string message) : base(message)
+        {
+        }
+
+        public VuelingExamInfrastructureException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
