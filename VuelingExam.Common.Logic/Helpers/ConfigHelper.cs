4b4f3f333b31d75beb4a2b22a8bbef43fc9589da
Ferran Ramírez <ferranramireznavajon@gmail.com>
2019-03-28 09:43:29 +0100

    Add project files.

---
+++ VuelingExam.Common.Logic/Helpers/ConfigHelper.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using VuelingExam.Common.Logic.Resources;
+
+namespace VuelingExam.Common.Logic.Helpers
+{
+    public static class ConfigHelper
+    {
+        public static IConfiguration AppSettings
+        {
+            get
+            {
+                var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+                return builder.Build();
+            }
+        }
+
+        public static void CleanDatabase()
+        {
+            string connectionString = ConfigHelper.AppSettings["ConnectionString"];
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string queryString = DatabaseResources.DeleteTables;
+                using (SqlCommand command = new SqlCommand(queryString, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
---
+++ VuelingExam.Common.Mapper/Mapper.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using System.Collections.Generic;
+
+namespace VuelingExam.Common.Mapper
+{
+    public class Mapper
+    {
+        public U Map<T, U>(T origin)
+        {
+            IMapper mapper = MapCreator<T, U>();
+            return mapper.Map<T, U>(origin);
+        }
+        public List<U> MapList<T, U>(List<T> origin)
+        {
+            IMapper mapper = MapCreator<T, U>();
+            return mapper.Map<List<T>, List<U>>(origin);
+        }
+        public Dictionary<V, List<W>> MapDictionary<T, U, V, W>(
+            Dictionary<T, List<U>> origin)
+        {
+            IMapper mapper = MapCreator<T, U>();
+            return mapper.Map<Dictionary<T, List<U>>,
+                Dictionary<V, List<W>>>(origin);
+        }
+
+        private static IMapper MapCreator<T, U>()
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<T, U>());
+            IMapper mapper = config.CreateMapper();
+            return mapper;
+        }
+    }
+}
