455e2c78999c339a8b38307952960463a487c6b4
Ferran Ramírez <ferranramireznavajon@gmail.com>
2019-03-28 14:07:17 +0100

    Rate repository unit tests #23

---
+++ VuelingExam.Infrastructure.Test.Unit/RateRepositoryTests.cs
@@ -0,0 +1,57 @@
+using Autofac.Extras.Moq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using VuelingExam.Infrastructure.Contracts.Repository;
+using VuelingExam.Infrastructure.DataModel;
+using VuelingExam.Infrastructure.Impl.Repository;
+
+namespace VuelingExam.Infrastructure.Test.Unit
+{
+    [TestClass]
+    public class RateRepositoryTests
+    {
+        RateRepository rateRepository;
+        List<RateDM> rateDMList, createdRateDMList;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            rateRepository = new RateRepository();
+            createdRateDMList = new List<RateDM>();
+            rateDMList = new List<RateDM>
+            {
+                new RateDM("RBR", "YEN", 15.55m),
+                new RateDM("EUR", "PES", 23.32m),
+                new RateDM("DOL", "COL", 0.25m)
+            };
+        }
+
+        [TestMethod]
+        public void ReadAllTest()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                mock.Mock<IRateRepository>().Setup(x => x.ReadAll()).Returns(rateDMList);
+                var sut = mock.Create<IRateRepository>();
+
+                var actual = sut.ReadAll();
+                mock.Mock<IRateRepository>().Verify(x => x.ReadAll());
+                Assert.AreEqual(rateDMList, actual);
+            }
+        }
+
+        [TestMethod]
+        public void CreateTest()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                mock.Mock<IRateRepository>().Setup(x => x.CreateAll(rateDMList)).Returns(rateDMList);
+                var sut = mock.Create<IRateRepository>();
+
+                var actual = sut.CreateAll(rateDMList);
+                mock.Mock<IRateRepository>().Verify(x => x.CreateAll(rateDMList));
+                Assert.AreEqual(rateDMList, actual);
+            }
+        }
+    }
+}
